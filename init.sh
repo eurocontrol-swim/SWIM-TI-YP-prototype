4a9fe7961bc819cdedc0692c9f9a880f00211d13
Alex Ntavelos <a.ntavelos@gmail.com>
2019-06-20 15:03:40 +0200

    fix readme

--- init.sh
+++ init.sh
@@ -2,6 +2,6 @@
 
 echo "Cloning repositories"
 
-git clone git clone https://antavelos-eurocontrol@bitbucket.org/antavelos-eurocontrol/subscription-manager.git &&
-git clone git clone https://antavelos-eurocontrol@bitbucket.org/antavelos-eurocontrol/swim-adsb.git &&
-git clone git clone https://antavelos-eurocontrol@bitbucket.org/antavelos-eurocontrol/swim-explorer.git
+git clone https://antavelos-eurocontrol@bitbucket.org/antavelos-eurocontrol/subscription-manager.git &&
+git clone https://antavelos-eurocontrol@bitbucket.org/antavelos-eurocontrol/swim-adsb.git &&
+git clone https://antavelos-eurocontrol@bitbucket.org/antavelos-eurocontrol/swim-explorer.git
